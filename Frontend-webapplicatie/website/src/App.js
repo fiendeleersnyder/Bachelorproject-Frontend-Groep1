a8639c3cdfe04a72251c5ae4be4f344983f5c3a5
EmmyValckenier <emmy.valckenier@gmail.com>
2022-02-22 11:32:29 +0100

    Poging 2

--- Frontend-webapplicatie/website/src/App.js
+++ Frontend-webapplicatie/website/src/App.js
@@ -8,9 +8,6 @@ function App() {
      //<title>Masterproeftool</title>
      //<meta charset="UTF-8">
      //<meta name="viewport" content="width=device-width, initial-scale=1">
-//     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-//     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-//     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 //     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     //</head>
     //<body>
@@ -25,12 +22,7 @@ function App() {
                   <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
                   <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
                 </div>
-//                <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
-//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
-//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
-//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
-//                </div>
+
             </div>
 
           </header>
@@ -38,6 +30,12 @@ function App() {
     //</body>
 
 //
+//                <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
+//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
+//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
+//                    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+//                </div>
 //    <!-- Header -->
 //    <header class="w3-container w3-red w3-center" style="padding:128px 16px">
 //      <h1 class="w3-margin w3-jumbo">START PAGE</h1>
