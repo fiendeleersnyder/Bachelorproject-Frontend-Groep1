deaa3d6de1fe0522d23b39a56ba86bbc32be81a6
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-03-10 12:40:36 +0100

    Poging 4.3

--- Frontend-webapplicatie/website/src/Pages/onderwerpen.js
+++ Frontend-webapplicatie/website/src/Pages/onderwerpen.js
@@ -1,19 +1,29 @@
-
-function OnderwerpenLijst() {
-  return(
-    <div>
-        <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
-        <p>HIER OOK</p>
-        <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
-        <p>HIER OOK</p>
-        <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
-        <p>HIER OOK</p>
-        <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
-        <p>HIER OOK</p>
-      {/*<Onderwerp text='Learn React' />*/}
-      {/*<Onderwerp text='Learn React' />*/}
-    </div>
-);
+import axios from 'axios';
+async function OnderwerpenLijst() {
+    const api = axios.create({
+        baseURL:'http://localhost:8080'
+    })
+    try {
+        let data = await api.get('/onderwerpen').then(({data}) => data);
+        this.setState({onderwerpen: data})
+    } catch (err) {
+        console.log(err)
+    }
+    return (
+        <div>
+            <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
+            <p>HIER OOK</p>
+            <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
+            <p>HIER OOK</p>
+            <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
+            <p>HIER OOK</p>
+            <p>HIER MOETEN ONDERWERPEN KOMEN TE STAAN</p>
+            <p>HIER OOK</p>
+            {/*<Onderwerp text='Learn React' />*/}
+            {/*<Onderwerp text='Learn React' />*/}
+            {this.state.onderwerpen.map(onderwerp => <h3 key={onderwerp.id}>{onderwerp.name}</h3>)}
+        </div>
+    );
 }
 
 export default OnderwerpenLijst;
