246911c2a8d73a0e0417dde95c3c5bb5aa2a5195
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-03-15 11:57:12 +0100

    Poging 5.2

--- Frontend-webapplicatie/website/src/Componenten/Menubalk.js
+++ Frontend-webapplicatie/website/src/Componenten/Menubalk.js
@@ -4,12 +4,12 @@ function Menubalk(){
       <header>
         <div class="w3-top">
             <div class="w3-bar w3-kulblauw w3-card w3-left-align w3-large">
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/'>Home</Link></a>
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/onderwerpen'>Onderwerpen</Link></a>
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/addonderwerp'>Onderwerp toevoegen</Link></a>
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/indienen'>Indienen</Link></a>
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/chat'>Chat</Link></a>
-              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-kulblauw"><Link to='/account'>Mijn account</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/'>Home</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/onderwerpen'>Onderwerpen</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/addonderwerp'>Onderwerp toevoegen</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/indienen'>Indienen</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/chat'>Chat</Link></a>
+              <a class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><Link to='/account'>Mijn account</Link></a>
             </div>
         </div>
       </header>
