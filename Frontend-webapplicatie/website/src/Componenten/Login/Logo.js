fbae6196216925f0c3bb04829b806a6816591738
EmmyValckenier <emmy.valckenier@gmail.com>
2022-04-19 09:25:21 +0200

    Poging 9

--- Frontend-webapplicatie/website/src/Componenten/Login/Logo.js
+++ Frontend-webapplicatie/website/src/Componenten/Login/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Image } from 'react-native'
 
 const Logo = () => (
-    <Image source = {require('C:\\Users\\User\\IdeaProjects\\Bachelorproject-Frontend-Groep1\\Frontend-webapplicatie\\website\\src\\Images\\logo_kuleuven.png')}
+    <Image source = {require('C:\\Users\\emmyv\\IdeaProjects\\Bachelorproject-Frontend-Groep1\\Frontend-webapplicatie\\website\\src\\Images\\logo_kuleuven.png')}
     style = {{width:200, height: 50}}/>
 )
 export default Logo
