fe26a04cf693b04954b99f0f9949106b69aba64d
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-05-15 13:41:04 +0200

    Login

--- Frontend-mobieleapplicatie/BachelorproefApp/App.js
+++ Frontend-mobieleapplicatie/BachelorproefApp/App.js
@@ -10,9 +10,11 @@ import Login from './app/components/Login/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function App() {
+  const accessToken = await AsyncStorage.getItem('accesToken');
 
   function Uitloggen(){
     return (
@@ -27,42 +29,46 @@ export default function App() {
 
     return (
       // <AppContainer />
-    <NavigationContainer>
-      <Tab.Navigator 
-          screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+      accessToken === undefined ? (
+      <Login/>
+      ) : (
+      <NavigationContainer>
+        <Tab.Navigator 
+            screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-              if (route.name === 'Home') {
-                iconName = focused
-                  ? 'home-sharp'
-                  : 'home-outline';
-              } else if (route.name === 'Subjects') {
-                iconName = focused ? 'albums' : 'albums-outline';
-              } else if (route.name === 'Submit') {
-                iconName = focused ? 'arrow-redo-circle' : 'arrow-redo-circle-outline';
-              } else if (route.name === 'Logout') {
-                iconName = focused ? 'exit' : 'exit-outline';
-              } else if (route.name === 'Add Subject') {
-                iconName = focused ? 'add-circle' : 'add-circle-outline';
-              }
-              
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: '#52BDEC',
-            tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Home" component={Homescreen} />
-        <Tab.Screen name="Subjects" component={Onderwerpenscreen} />
-        <Tab.Screen name="Add Subject" component={AddOnderwerpscreen} />
-        <Tab.Screen name="Submit" component={Indienenscreen} />
-        <Tab.Screen name="Logout" component={Uitloggen} />
-        <Tab.Screen name="Subject Details" component={Onderwerpendetailscreen} options={{tabBarButton: (props) => null}}></Tab.Screen>
-      </Tab.Navigator>
-      
-    </NavigationContainer>
+                if (route.name === 'Home') {
+                  iconName = focused
+                    ? 'home-sharp'
+                    : 'home-outline';
+                } else if (route.name === 'Subjects') {
+                  iconName = focused ? 'albums' : 'albums-outline';
+                } else if (route.name === 'Submit') {
+                  iconName = focused ? 'arrow-redo-circle' : 'arrow-redo-circle-outline';
+                } else if (route.name === 'Logout') {
+                  iconName = focused ? 'exit' : 'exit-outline';
+                } else if (route.name === 'Add Subject') {
+                  iconName = focused ? 'add-circle' : 'add-circle-outline';
+                }
+                
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#52BDEC',
+              tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="Home" component={Homescreen} />
+          <Tab.Screen name="Subjects" component={Onderwerpenscreen} />
+          <Tab.Screen name="Add Subject" component={AddOnderwerpscreen} />
+          <Tab.Screen name="Submit" component={Indienenscreen} />
+          <Tab.Screen name="Logout" component={Uitloggen} />
+          <Tab.Screen name="Subject Details" component={Onderwerpendetailscreen} options={{tabBarButton: (props) => null}}></Tab.Screen>
+        </Tab.Navigator>
+        
+      </NavigationContainer>
+      )
   );
 }
 
